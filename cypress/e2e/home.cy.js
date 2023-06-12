c7e00d1ca6800eeac8f03d292d9c50aaf700e7d2
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-06-12 12:33:22 -0400

    Upadte home test user should see title, urls on load

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,29 @@
+describe('Url Home Page', () => {
+  beforeEach(() => {
+    cy.fixture('url.json').as('urls');
+    cy.intercept('GET', 'http://localhost:3001/api/v1/urls', { fixture: 'url.json' });
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should have a page title', () => {
+    cy.title().should('eq', 'URL Shortener');
+  });
+
+  it('should have existing URLs on the home page', () => {
+    cy.get('.url').should('have.length', 3);
+  
+    cy.get('.url').first().within(() => {
+      cy.get('h3').should('contain', 'Awesome photo');
+      cy.get('a').should('have.attr', 'href', 'http://localhost:3001/useshorturl/1');
+      cy.get('a').should('contain', 'http://localhost:3001/useshorturl/1');
+      cy.get('p').should('contain', 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80');
+    });
+  
+    cy.get('.url').last().within(() => {
+      cy.get('h3').should('contain', 'Wicked photo');
+      cy.get('a').should('have.attr', 'href', 'http://localhost:3001/useshorturl/3');
+      cy.get('a').should('contain', 'http://localhost:3001/useshorturl/3');
+      cy.get('p').should('contain', 'https://images.pexels.com/photos/35888/amazing-beautiful-breathtaking-clouds.jpg?auto=compress&cs=tinysrgb&w=1600');
+    });
+  });
+});
